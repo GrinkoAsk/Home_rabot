dddd071298274c1974dea84b71dcb63cb9fc340a
GrinkoAnastasia <knasvskaiask27@gmail.com>
2023-01-30 00:48:35 +0300

    13,15,27,777

--- Examples002/Program27.cs
+++ Examples002/Program27.cs
@@ -1,38 +1,26 @@
-/* Задача 27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-452 -> 11;  82 -> 10;  9012 -> 12   */
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
 
-int number = ReadInt("Введите число: ");
-
-int len = NumberLen(number);
-SumNumbers(number, len);
-
-// Функция подсчета цифр в числе
-int NumberLen(int a)
+int GetNumberByUser(string text)                      // Получили число от пользователя
 {
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
-}
-
-// Функция вывода суммы цифр в числе
-void SumNumbers(int n, int len)
-{
-    int sum = 0;
-    for (int i = 1; i <= len; i++)
-    {
-        sum += n % 10;
-        n /= 10;
-    }
-    Console.WriteLine($"сумма цифр {sum}");
-}
-
-// Функция ввода
-int ReadInt(string message)
-{
-    Console.Write(message);
+    Console.WriteLine(text + ": ");
     return Convert.ToInt32(Console.ReadLine());
 }
+
+int SumOfNumbers(int countByUser)                     // Разбили по разрядам , переведя в строку
+{
+  int result = 0;
+
+    while (countByUser > 0)
+    {
+      result += countByUser % 10;
+      countByUser /= 10;
+    }
+return result;
+}
+
+int countByUser = GetNumberByUser("Введите число, если хочешь узнать сумму его цифр");
+int sum = SumOfNumbers(countByUser);
+Console.WriteLine(sum);
--- Examples002/Program777.cs
+++ Examples002/Program777.cs
@@ -1,55 +1,72 @@
 //Задача 777;
 using static Library;
+// // Напишите программу, которая выводит массив из N элементов,
+// // заполненный нулями и единицами в случайном порядке.
+// // Самое левое число != 0
+// // Данный массив есть двоичное представление десятичного числа
+// // [1,0,1,1,0,1,0,0]
+// // Реализовать перевод двоичного числа в десятичное
 
-// Напишите программу, которая выводит массив из N элементов,
-// заполненный нулями и единицами в случайном порядке.
-int n = 8;
-int[] arr = новый int[n];
-Случайный ранд = новый случайный();
-для (int i = 0; Я < обр. Длина; я++)
-{
-    arr[i] = rand. Далее(0,2);
-}
-Console.WriteLine(строка. Join(", ", arr));
-// Самое левое число != 0
-// Данный массив есть двоичное представление десятичного числа
-// [1,0,1,1,0,1,0,0]
-// Реализовать перевод двоичного числа в десятичное
-int main(void)
-{
-    char q;
-    int res = 0;
-    while(scanf("%c", &q)==1)
-    {
-        if (q != '0' && q != '1')
-        {
-            if (q == '\n') break; // Конец ввода
-        }
-        res = res*2+(q-'0');
-    }
-    printf("%i\n", res);
-// 1. Ввод N
-// int <- (string)
+// int GetNumberByUser(string text)                   // Считать данные от пользователя
+// {
+//     Console.WriteLine(text + ": ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
 
-// 2. Создание массива
-// int[] <- (int)
+// int[] CreateArray(int size)                       // Создание массива
+// {
+//    return new int[size+1]; 
+// }
+ 
+// void FillArray(int[] collection)                  // Метод заполнения массива случайными числами от 0 до 1
+// {
+//     int length = collection.Length;
+//     int index = 1;
+//     collection[0] = 1;
+//     while (index < length)
+//     {
+//         collection[index] = Random.Shared.Next(2); 
+//         index++;
+//     }
+// }
 
-// 3. Заполнение массива 0 и 1
-// void <- (int[])
+// string PrintArray(int[] collection)               // Метод печати массива
+// {
+//     string output = String.Empty;
+//     int size = collection.Length;
+//     for (int i = 0; i < collection.Length; i++)
+//     {
+//         output = output + $"{collection[i]} ";
+//     }
+//     return output;
+// } 
 
-// 4. Печать массива
-// string <- (int[])
+// int toDecimal(int[] collection)                   // Переводим в десятичную из двоичной
+// {
+// int count = collection.Length;
+// int exponent = collection.Length - 1; 
+// int result = 0;
+// for (int i = 0; i < collection.Length; i++)
+// {
+//     if (collection[i] == 1)
+//     {
+//         result += Convert.ToInt32(Math.Pow(2, exponent));
+//     }
+//     exponent--;
+// }
+// return result;
+// }
 
-// 5. Конвертация из 2-ого в 10-ое
-// int <- (int[])
+// string GoodPrint(int[] collection, int dec)       // Красиво печатаем результат
+// {
+//     return $"{String.Join("", collection)} >> {dec}";
+// }
 
-// 6. 10110100 >> 180
-// string <- (int[] , int)
-string GoodPrint(int[] bin, int dec)
-{
-return $"{String.Join("", bin)} >> {dec}";
-}
-
-Console.WriteLine(GoodPrint(new int[] { 1, 0, 1, 1, 1, 1, 0 }, 56));
-
-//ДОПОЛНИТЕЛЬНАЯ ПОЛНЫЙ ЦИКЛ РЕШЕНИЯ С РАЗБИВКОЙ НА МЕТОДЫ КАК В ПРЕДЫДУЩЕЙ ЗАДАЧЕ
+int countByUser = GetNumberByUser("Введите число");
+int[] array = CreateArray(countByUser);
+FillArray(array);                                   // Вызов метода заполнения  массива
+PrintArray(array);                                  // Вызов метода печати массива
+int result = toDecimal(array);                      // Кладем результат конвертации в int
+string decNumber = PrintArray(array);               // Делаем строку из результата распечатки
+Console.WriteLine(decNumber);            
+Console.WriteLine(GoodPrint(array, result));
